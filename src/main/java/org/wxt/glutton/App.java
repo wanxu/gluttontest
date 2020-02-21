7dd141d0e51402a61ee1a9890f72249af80efa71
wanxu <wangxutongtony@163.com>
2020-02-21 11:33:41 +0800

    初始化

---
+++ src/main/java/org/wxt/glutton/App.java
@@ -0,0 +1,13 @@
+package org.wxt.glutton;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
